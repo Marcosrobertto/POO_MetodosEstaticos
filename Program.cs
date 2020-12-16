8d55cf1be1a860532591d92f56257dea30cd8fa0
Marcos Roberto <marcosrobertto46@gmail.com>
2020-12-16 14:48:19 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using POO_MetodosEstaticos.classes;
+
+namespace POO_MetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(2,5));
+
+            Console.WriteLine("Digite um valor");
+            Conversor.valorUsuario = float.Parse(Console.ReadLine());
+            Console.WriteLine("valor de dolar para real é : ");
+            Console.WriteLine(Conversor.ConverterDolarParaReal());
+            Console.WriteLine("valor de real para dolar é : ");
+            Console.WriteLine(Conversor.ConverterRealParaDolar());
+            Console.WriteLine();
+            Console.WriteLine("valor de euro para para real é : ");
+            Console.WriteLine(Conversor.CoverterEuroParaReal());
+            Console.WriteLine("valor de real para euro é : ");
+            Console.WriteLine(Conversor.ConverterRealParaEuro());
+
+
+        }
+    }
+}
